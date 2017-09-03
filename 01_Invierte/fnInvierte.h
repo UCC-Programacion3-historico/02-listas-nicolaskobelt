eadcc966f7b5f49784af7e9572e47f271b5154a6
nicolaskobelt <kobeltnicolas@me.com>
2017-09-03 09:49:35 -0300

    invertir casi ok

--- 01_Invierte/fnInvierte.h
+++ 01_Invierte/fnInvierte.h
@@ -3,13 +3,37 @@
 #define FNINVIERTE_H
 
 
-template <class T>
-void fnInvierte (Lista<T> *lis);
+class {
+private:
+    nodix<T> *inicio;
+public:
+    void fnInvierte(Lista<T> *lis);
 
+};
 
-template <class T>
 void fnInvierte (Lista<T> *lis){
+    nodo<T> *aux = incio;
+    int *anterior, *siguiente;
 
+    unsigned int pos=0;
+
+    while (siguente != NULL) {
+
+        if (pos == 0) {
+            siguiente = aux->getNext();
+            aux = aux->setNext(NULL);
+        } else {
+            anterior = aux;
+            aux = aux->getnext();
+            siguiente = aux;
+
+            aux->setNext(anterior);
+
+            }
+
+        pos ++;
+
+    }
 }
 
 
