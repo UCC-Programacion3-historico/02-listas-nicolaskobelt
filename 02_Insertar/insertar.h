095e0e82bda0f6772c007ff2b6fb14b0d7d4e6d8
nicolaskobelt <kobeltnicolas@me.com>
2017-09-06 19:35:27 -0300

    Solucionado a lo aborigen

--- 02_Insertar/insertar.h
+++ 02_Insertar/insertar.h
@@ -1,12 +1,16 @@
-#include <iostream>
+#include "../Lista/Lista.h"
+#include "../Lista/nodo.h"
 #ifndef INC_02_LISTAS_INSERTAR_H
 #define INC_02_LISTAS_INSERTAR_H
 
-
+template <class T>
 class insert {
 private:
     nodo<T> *inicio;
+public:
+
 };
+
 
 
 
--- 02_Insertar/main.cpp
+++ 02_Insertar/main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
 #include "../Lista/Lista.h"
+using namespace std;
 
+template <class T>
 int main() {
-    std::cout << "Ejercicio 01/02\n" << std::endl;
+    int n,m;
+
+    cout<<"programita para meter numeros"<<endl;
+
+    cout<<"Indicar posicion del dato: 1=principio, 2=diome, 3=final: ";
+    cin>>n; cout<<endl;
+
+    cout<<"Dato a ingresar: ";
+    cin>>m; cout<<endl;
+
+    Lista<T> listita;
+
+    switch (n){
+        case 1:
+            listita.insertarPrimero(m);
+            break;
+        case 2:
+            listita.insertar(pos, m);
+            break;
+        case 3:
+            listita.insertarUltimo(m);
+            break;
+        default:
+            cout<<"Pos incorrecta"<<endl;
+            break;
+
+    }
+    
+
+
     return 0;
 }
