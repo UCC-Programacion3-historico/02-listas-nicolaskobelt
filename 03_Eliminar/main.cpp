b8fea3074418ffd8df69cc36a68bbbb2f4969421
nicolaskobelt <kobeltnicolas@me.com>
2017-09-16 18:39:30 -0300

    Ej 3 OK

--- 03_Eliminar/main.cpp
+++ 03_Eliminar/main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
 #include "../Lista/Lista.h"
+using namespace std;
+
+
+template<class T>
 
 int main() {
-    std::cout << "Ejercicio 01/03\n" << std::endl;
-    return 0;
+    Lista<T> li;
+
+    int n, m, dato, borrar;
+
+    cout<<"Ingrese cantidad de datos: "<<endl;
+    cin>>n;
+
+    //ingresar los datos
+    int pos = 0;
+    while (pos != n){
+        pos++;
+
+        cout<<"Ingrese valor ["<<pos<<"]"<<endl;
+        cin>>m;
+
+        if(pos==1)
+            li.insertarPrimero(m);
+        else
+            li.insertar(pos,m);
+    }
+
+    //mostrar los datos ingresados
+    pos = 0;
+    while (pos != n ){
+        dato = li.getDato(pos);
+        pos++;
+        cout<<"Posicion ["<<pos<<"] Dato: "<<dato<<endl;
+    }
+
+
+    cout<<"Ingrese posicion de dato a borrar: ";
+    cin>>borrar;
+    cout<<endl;
+    li.remover(borrar);
+
+    //muestro nuevamente toda la lista
+    pos = 0;
+    while (pos != n ){
+        dato = li.getDato(pos);
+        pos++;
+        cout<<"Posicion ["<<pos<<"] Dato: "<<dato<<endl;
+    }
+
 }
